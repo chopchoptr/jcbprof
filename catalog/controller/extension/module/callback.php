1825982388b93c1f27de0f6da19901a59aa5a39f
br0nebr0 <grechihin.andrey@gmail.com>
2020-08-18 15:15:37 +0300

    add SEO support for callback form

--- catalog/controller/extension/module/callback.php
+++ catalog/controller/extension/module/callback.php
@@ -7,29 +7,24 @@ class ControllerExtensionModuleCallback extends Controller
         if ($this->request->server["REQUEST_METHOD"]=='POST')
         {
            $num = $this->model_extension_module_callback->RecordData();
-            if ($_GET && isset($_GET['product_id']))
-            $str = "product_id=".$_GET['product_id'];
-            else
-            $str = "";
-            if ($num['id'] > 0)
+            $str = $_SERVER['REQUEST_URI'];
+            $href = parse_url($_SERVER['REQUEST_URI'])['path'];
+            if (isset(parse_url($_SERVER['REQUEST_URI'])['query']))
                 $str.="&ticket=".$num['id'];
-                $route = isset($_GET['route']) ? $_GET['route'] : "common/home";
-        $this->response->redirect( $this->url->link($route, $str, true));
+            else
+            $str = "ticket=".$num['id'];
+            $route = isset($_GET['route']) ? $_GET['route'] : "common/home";
+            if( $href != "/index.php" && $href != "/")
+                $this->response->redirect($href."?".$str);
+            else
+                $this->response->redirect($str);
         }
         $data = array();
-        if (isset($_GET))
-        {
-            $route = isset($_GET['route']) ? $_GET['route'] : NULL;
-            if ($route == "product/product")
-                $data['product_id'] = $_GET['product_id'];
-            $ticket = isset($_GET['ticket']) ? $_GET['ticket'] : "";
-            if ((int) $ticket > 0)
-            {
-                $data['ticket'] = $ticket;
-                $data['route'] = $route;
-                $data['url'] = parse_url($_SERVER['REQUEST_URI'])['path'];
-            }
-        }
+        $route = isset($_GET['route']) ? $_GET['route'] : NULL;
+        $data['product_id'] = $_SERVER['REQUEST_URI'];
+        $ticket = isset($_GET['ticket']) ? $_GET['ticket'] : "";
+            $data['ticket'] = $ticket;
+        $data["ticket_url"] =  str_replace(isset($_GET['route']) ? "&ticket=".$ticket : "ticket=".$ticket, "", $_SERVER['REQUEST_URI']);
         $data += $this->load->language('extension/module/callback');
         return $this->load->view('extension/module/callback', $data);
     }
--- catalog/model/extension/module/callback.php
+++ catalog/model/extension/module/callback.php
@@ -7,6 +7,7 @@ class ModelExtensionModuleCallback extends Model
       if ($this->request->post['action'] == "send_callback")
       {
         $data = $this->request->post;
+        
         if (empty($data['consumer_name']) || empty($data['consumer_tel']))
           return (['id' => 0]);
         date_default_timezone_set("Europe/Moscow");
@@ -18,12 +19,9 @@ class ModelExtensionModuleCallback extends Model
         $data['consumer_text'] = $this->defend_str($data['consumer_text']);
         $data['consumer_tel'] = $this->validate_tel($data['consumer_tel']);
         $data['consumer_mail'] = $this->validate_mail($data['consumer_mail']);
-        if (!empty($data['product_id']))
-          $data['product_id'] = abs(intval($data['product_id']));
         $this->db->query("INSERT INTO `oc_callback`(`name`, `phone`, `mail`, `vin`, `product_id`, `quest`, `date`) VALUES ('".$data['consumer_name'] ."','".$data['consumer_tel'] ."','".$data['consumer_mail'] ."','".$data['consumer_vin'] ."','".$data['product_id'] ."','".$data['consumer_text'] ."', '". $time ."')");
-        $query = $this->db->query("SELECT id FROM `oc_callback` WHERE `date` = '".$time."'");
+        $query = $this->db->query("SELECT `id`, `product_id` FROM `oc_callback` WHERE `date` = '".$time."'");
         return ($query->row);
-        return(['id' => 0]);
       }
       return ("");
     }
