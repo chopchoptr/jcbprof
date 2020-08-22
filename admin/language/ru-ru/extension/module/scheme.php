3cf82e3bfa6edf8e2a52c94d359a4c7a792eb8df
br0nebr0 <grechihin.andrey@gmail.com>
2020-08-22 20:56:22 +0300

    Довел фичу до ума, можно дизайнить и верстать

--- admin/language/ru-ru/extension/module/scheme.php
+++ admin/language/ru-ru/extension/module/scheme.php
@@ -1,25 +1,44 @@
 <?php
-
-$_['heading_title']    = 'JCB интерактивные схемы НЕ ВКЛЮЧАТЬ!!';
-
+//all
+$_['heading_title']    = 'JCB интерактивные схемы';
+$_['settings'] = 'Настройки';
+$_['schemes'] = 'Схемы';
 $_['scheme_cat'] = 'Категории схем';
-$_['scheme_cat_id'] = "id категории";
-$_['scheme_cat_desc'] = "Описание категории";
-
-$_['scheme_id'] = "№ схемы";
-$_['scheme_image'] = "Изображение схемы";
-$_['scheme_title'] = "Название схемы";
-$_['scheme_engine'] = "Двигатель схемы";
-
-$_['scheme_point_id'] = "Номер схемы";
-$_['scheme_point_desc'] = "Описание точки";
-$_['scheme_point_filter'] = "Фильтр товаров";
-
-$_['text_extension'] = 'Расширения';
-$_['text_success']   = 'Настройки успешно изменены!';
-$_['text_edit']      = 'Текущие заявки';
+$_['text_scheme'] = 'Интерактивные схемы';
+$_['text_action'] = "Действие";
+$_['text_action_change'] = "Изменить";
+$_['text_action_add'] = "Добавить";
+$_['text_action_remove'] = "Удалить";
+//options
+$_['text_edit_options'] = "Настройки модуля";
 $_['entry_status']   = 'Статус';
 $_['text_enabled']   = 'Включено';
 $_['text_disabled']  = 'Выключено';
-
+$_['text_success']   = 'Настройки успешно изменены!';
+//categories
+$_['categories_title'] = "Редактирование категорий";
+$_['scheme_cat_name'] = "Название категории";
+$_['scheme_cat_desc'] = "Описание категории";
+$_['new_cat_placeholder'] = "Новая категория";
+//schemes
+$_['schemes_title'] = "Редактирование схем";
+$_['scheme_title'] = "Редактирование схемы";
+$_['scheme_id'] = "№";
+$_['scheme_image'] = "Изображение";
+$_['scheme_name'] = "Название";
+$_['scheme_cat'] = "Категория";
+$_['scheme_engine'] = "Двигатель";
+$_['universal_engine'] = "Универсальный";
+$_['text_action_set_points'] = "Установить точки";
+//scheme
+$_['scheme_point_desc'] = "Описание точки";
+$_['scheme_point_filter'] = "Фильтр товаров";
+$_['text_action_set_filt'] = "Установить фильтр";
+$_['text_save_list'] = "Сохранить список";
+$_['text_rem_ch'] = "Удалить выбранное";
+//point
+$_['point_title'] = "Редактирование фильтров";
+$_['text_filter_group'] = "Группа фильтров";
+$_['text_filter'] = "Значение фильтров";
+$_['renew_filter_list'] = "Обновить фильтры";
 ?>
