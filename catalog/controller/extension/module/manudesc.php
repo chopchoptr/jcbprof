6111d111af6ca881db7e9a84fb588d572edc9fc8
br0nebr0 <grechihin.andrey@gmail.com>
2020-09-02 20:20:18 +0300

    rebuild cat btns

--- catalog/controller/extension/module/manudesc.php
+++ catalog/controller/extension/module/manudesc.php
@@ -12,7 +12,6 @@ class ControllerExtensionModuleManudesc extends Controller
         $data['content_bottom'] = $this->load->controller('common/content_bottom');
         $data['header'] = $this->load->controller('common/header');
         $data['footer'] = $this->load->controller('common/footer');
-
         $this->response->setOutput($this->load->view('extension/module/manudesc', $data));
     }
 }
