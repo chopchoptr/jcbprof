71822af4d5f8adf4e9acc8c684e043ddc3b79295
br0nebr0 <grechihin.andrey@gmail.com>
2020-09-08 18:10:47 +0300

    change limit on category

--- catalog/controller/product/category.php
+++ catalog/controller/product/category.php
@@ -291,7 +291,7 @@ class ControllerProductCategory extends Controller {
 
 			$data['limits'] = array();
 
-			$limits = array_unique(array($this->config->get('theme_' . $this->config->get('config_theme') . '_product_limit'), 25, 50, 75, 100));
+			$limits = array_unique(array($this->config->get('theme_' . $this->config->get('config_theme') . '_product_limit'), 32, 48, 64, 128));
 
 			sort($limits);
 
